bd42d1c1d6448ac8e1365c2fbc6abef1f5d3ee9e
Einar Kristiansen <einarr.kristiansen@outlook.com>
2019-05-02 13:35:01 +0200

    Initial scripts

---
+++ Export-RDCServerList/Export-RDCServerList.psm1
@@ -0,0 +1,5 @@
+﻿function Export-CybRDCServerList {
+$RDCManList = Get-Content '\\..\RDCMan.rdg'
+$Servers = $RDCManList | Select-String "<name>" | Select-String -Pattern ".+\:.+|MainWorkspace" -NotMatch 
+$Servers -replace "<name>" -replace "</name>" -replace " " | Out-File \\RDCMan\RDC_ServerList.txt -Force
+}
